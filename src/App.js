25d29fb3b4db91ffd5238340ccddd2e6ddb99cba
Kailee Smith <kaileesmith1501@gmail.com>
2021-03-04 20:35:46 -0800

    added start to footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <h1>Hello World</h1>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark">
+            <div className="container">
+                <span className="footerFont">Created By Kailee Smith</span>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
