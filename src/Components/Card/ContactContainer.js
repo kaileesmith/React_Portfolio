07f9a641ebe1119a3161ba441398860bdb35238a
Kailee Smith <kaileesmith1501@gmail.com>
2021-03-14 22:53:33 -0700

    added changes to contact page and fixed styling

--- src/Components/Card/ContactContainer.js
+++ src/Components/Card/ContactContainer.js
@@ -3,23 +3,27 @@ import React from 'react';
 const ContactContainer = () => {
     return (
         <div className= "contactCont">
-                    <div className="jumbotron">
+                <div className="jumbotron">
                     <div class="container">
-                    <h4 className="contactTitle">Feel Free to Reach out!</h4>
-                        <main className="col-md-12">
-                        <h5>Email: Kaileesmith1501@gmail.com</h5>
-                        <h5>Phone: (951)970-1909</h5>
+                        <h4 className="contactTitle">LETS WORK TOGETHER</h4>
+                        <main>
+                            <h5>Email: Kaileesmith1501@gmail.com</h5>
+                            <h5>Phone: (951) 970-1909</h5>
+                            <div className="links">
+                                <ul className="linkedin">
+                                    <a href="https://www.linkedin.com/in/kaileesmith/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-3x"></i></a>
+                                </ul>
+                                <ul className="github">
+                                    <a href="https://www.github.com/kaileesmith" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-3x"></i></a> 
+                                </ul>
+                                <ul>
+                                <a href="" target="_blank" rel="noreferrer"><i className="fas fa-file-download fa-3x"></i></a> 
+                                </ul>
+                            </div>
                         </main>
-                        <ul className="linkedin">
-                        <a href="https://www.linkedin.com/in/kaileesmith/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-3x"></i></a>
-                        </ul>
-                        <ul className="github">
-                        <a href="https://www.github.com/kaileesmith" target="_blank" rel="noreferrer"><i className="fab fa-github-square fa-3x"></i></a> 
-                        </ul>
-                        </div>
                     </div>
                 </div>
-            // </div>
+        </div>
     )
 }
 
